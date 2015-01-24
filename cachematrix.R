e621921a2860c8648b4501b0a37c2f9848c8bc76
Thai Thien <tthien@apcs.vn>
2015-01-24 16:20:20 +0700

    done assignment2

---
+++ cachematrix.R
@@ -0,0 +1,41 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+#copy from example
+#change "solve" to "solve"
+
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+            set <- function(y) {# set cache matrix
+                    x <<- y
+                    m <<- NULL
+            }
+            get <- function() x# get cache matrix
+            setsolve <- function(solve) m <<- solve #set cache matrix solve
+            getsolve <- function() m #get cache matrix solve
+            list(set = set, get = get,
+                 setsolve = setsolve,
+                 getsolve = getsolve)
+}
+
+
+## Write a short comment describing this function
+#copy from example
+#change "solve" to "solve"
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+		       m <- x$getsolve()
+            if(!is.null(m)) {#if m which is cache is not empty
+                    message("getting cached data")#read cache
+                    return(m)#done
+            }
+			
+			# solve the matrix
+            data <- x$get()
+            m <- solve(data, ...)
+            x$setsolve(m)#set in cache
+            m
+}
+
+# in my work, I use some material from Example: Caching the Mean of a Vector which this course have provided
